3455afa5880703a29a2fc8e77b674db18c14f596
Christian <CDuernberger8463@cosmoconsult.com>
2023-02-15 17:40:43 +0100

    initial

---
+++ main.js
@@ -0,0 +1,134 @@
+'use strict';
+
+/*
+ * Created with @iobroker/create-adapter v2.1.1
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require('@iobroker/adapter-core');
+const express = require('express');
+const httpTerminator = require('lil-http-terminator');
+
+// Load your modules here, e.g.:
+// const fs = require("fs");
+
+class Myhabitat extends utils.Adapter {
+
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: 'myhabitat',
+        });
+        this.on('ready', this.onReady.bind(this));
+        this.on('stateChange', this.onStateChange.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        // Initialize your adapter here
+        var app = express();
+        var server = require('http').createServer(app);
+        var port = 3000;
+
+        // Reset the connection indicator during startup
+        this.setState('info.connection', false, true);
+
+        // Mit diesem Kommando starten wir den Webserver.
+        server.listen(port, () => {
+            // Wir geben einen Hinweis aus, dass der Webserer läuft.
+            this.log.info(`Webserver läuft und hört auf Port ${port}`);
+            this.setState('info.connection', true, true);
+        });
+
+        this.log.error(__dirname);
+        app.use(express.static(__dirname + '/public'));
+
+        this.httpTerminator = httpTerminator({ server, gracefulTerminationTimeout: 2000, maxWaitTimeout: 10000 });
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            if(this.httpTerminator)
+                this.httpTerminator.terminate().finally(() => {
+                callback();
+                });
+            else
+                callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
+    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
+    // /**
+    //  * Is called if a subscribed object changes
+    //  * @param {string} id
+    //  * @param {ioBroker.Object | null | undefined} obj
+    //  */
+    // onObjectChange(id, obj) {
+    //     if (obj) {
+    //         // The object was changed
+    //         this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+    //     } else {
+    //         // The object was deleted
+    //         this.log.info(`object ${id} deleted`);
+    //     }
+    // }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    onStateChange(id, state) {
+        if (state) {
+            // The state was changed
+            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+        } else {
+            // The state was deleted
+            this.log.info(`state ${id} deleted`);
+        }
+    }
+
+    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
+    // /**
+    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
+    //  * @param {ioBroker.Message} obj
+    //  */
+    // onMessage(obj) {
+    //     if (typeof obj === 'object' && obj.message) {
+    //         if (obj.command === 'send') {
+    //             // e.g. send email or pushover or whatever
+    //             this.log.info('send command');
+
+    //             // Send response in callback if required
+    //             if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+    //         }
+    //     }
+    // }
+
+}
+
+if (require.main !== module) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new Myhabitat(options);
+} else {
+    // otherwise start the instance directly
+    new Myhabitat();
+}
